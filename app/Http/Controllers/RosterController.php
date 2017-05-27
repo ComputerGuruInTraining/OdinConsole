01408a66c15227ce306ebd7cdef63d7d29348e80
ComputerGuruInTraining <bernadettecar77@hotmail.com>
2017-05-27 13:03:26 +1000

    Grouped roster layout before alternate rows

--- app/Http/Controllers/RosterController.php
+++ app/Http/Controllers/RosterController.php
@@ -284,63 +284,108 @@ class RosterController extends Controller
 //        }
 //        echo "<script>console.log( 'Grouped Array: Index = " . $groupedJobs . "' );</script>";
 //
-        foreach($jobs as $job) {
-////            $uniqueDate = $job->uniqueDate;//many different values
-////            if($uniqueDate != null) {
-//                foreach ($groupedJobs->get($job->uniqueDate) as $shift) {
-//                    echo "<script>console.log( 'Shift Employee Name = " . $shift->employeeName . "' );</script>";
-//                    echo "<script>console.log( 'Shift Employee Key = " . $shift . "' );</script>";
-//
-//                    echo "<script>console.log( 'Shift Employee Key = " . $shift . "' );</script>";
-//                    $this->count = collect();
-//                    foreach($this->count as $counts) {
-//                        if ($shift->uniqueDate == $shift->startDate) {
-//                            $counts++;
-//                            echo "<script>console.log( Count = " . $counts . "' );</script>";
-//
-//
-//                        }
-//                    }
-//
-//                }
+
+//////            $uniqueDate = $job->uniqueDate;//many different values
+//////            if($uniqueDate != null) {
+////                foreach ($groupedJobs->get($job->uniqueDate) as $shift) {
+////                    echo "<script>console.log( 'Shift Employee Name = " . $shift->employeeName . "' );</script>";
+////                    echo "<script>console.log( 'Shift Employee Key = " . $shift . "' );</script>";
 ////
+////                    echo "<script>console.log( 'Shift Employee Key = " . $shift . "' );</script>";
+////                    $this->count = collect();
+////                    foreach($this->count as $counts) {
+////                        if ($shift->uniqueDate == $shift->startDate) {
+////                            $counts++;
+////                            echo "<script>console.log( Count = " . $counts . "' );</script>";
+////
+////
+////                        }
+////                    }
+////
+////                }
+//////
 //        }
 //////        $allJobs = $groupedJobs->all();//returns the underlying array from a collection
 ////        echo "<script>console.log( 'Grouped Array: Index = ".$allJobs['05/02/2017']. "' );</script>";
-        $count = 0;
-        foreach($groupedJobs as $index => $formattedJob) {
+//        $count = 0;
+        //$object => string)||($string => $value(dt??)
+
+//            foreach()
 ////            $locations = 'locations';
 ////            $assigned_user_id = 'assigned_user_id';
 //
-
-
-
-//            //conclusion: accesses $formattedJob at first index (with id = 2) but won't access the other indexes.
-            if ($index == $job->uniqueDate) {
-                echo "<script>console.log( 'Group Indexes = " . $index . "' );</script>";
-////                echo "<script>console.log( 'Grouped Looped = " . $formattedJob->find(2) . "' );</script>";//returns a collection instance with id = 2 if 2 passed as argument, nothing when 79 is although id = 79
-//////                echo "<script>console.log( 'Grouped Property = " . $formattedJob->all('locations') . "' );</script>";//returns a collection instance with id = 2 if 2 passed as argument, nothing when 79 is although id = 79
-////
-////            } else {
-////                echo "<script>console.log( 'Grouped Property not found' );</script>";//returns a collection instance with id = 2 if 2 passed as argument, nothing when 79 is although id = 79
-////
-////
-            }
-            if($index == '05/28/2017'){
-//                echo "<script>console.log( 'Accessed Index = " . $index . "' );</script>";
-                $count++;
-
-//            $count = count($index == '05/03/2017');
-                echo "<script>console.log( 'Count of indexes = " . $count . "' );</script>";
-
-
-            }
-        }
+        $count = 0;
+        $count2 = 0;
+        $count3 = 0;
+//        foreach($jobs as $job) {
+////            if($job->uniqueDate )
+//            foreach ($groupedJobs->get($job->startDate) as $shift) {
 //
-        }
+//                foreach($groupedJobs as $index => $formattedJob) {
+//                    if($index == $shift->startDate) {
+//                        $count++;
+//                        echo "<script>console.log( 'Shift Start Date = " . $shift->startDate . "' );</script>";
+//                        echo "<script>console.log( 'Shift Start Date = " . $count . "' );</script>";//79 if unique Date 7
+//                    }
+//                }
+//
+//            }
+//        }
+//        foreach($jobs as $job) {
+            foreach($groupedJobs as $index => $formattedJob) {
+//            if($job->uniqueDate )
 
-        return $groupedJobs;
-    }
+                echo "<script>console.log( 'Index = " . $index . "' );</script>";
+
+            foreach ($groupedJobs->get($index) as $shift) {
+                if($index == $shift->startDate) {
+
+
+                    $count2++;
+                echo "<script>console.log( 'Data in 2nd loop = " . $shift->startDate . "' );</script>";
+
+                echo "<script>console.log( 'count2 = " . $count2 . "' );</script>"; //7 but no results coming up below from 3-7./30
+
+//                    $count3++;
+//                echo "<script>console.log( 'Data in 3rd loop = " . $shift->startDate . "' );</script>";
+
+                echo "<script>console.log( 'count3 = " . $count3 . "' );</script>";//when count 1, 23. when count 2, 46 then 3-7 no results.//30
+//                    if($index == $shift->uniqueDate) {
+////                        foreach($jobs as $job){
+////                            $count++;
+////                            echo "<script>console.log( 'Data in last loop = " . $shift->startDate . "' );</script>";
+////                            echo "<script>console.log( 'count1 = " . $count . "' );</script>";//when count 1, 23. when count 2, 46 then 3-7 no results.
+////                        }
+                    }
+                }
+
+            }
+//        }
+//
+
+
+return $groupedJobs;
+}
+//            //conclusion: accesses $formattedJob at first index (with id = 2) but won't access the other indexes.
+//            if ($index == $job->uniqueDate) {
+//                echo "<script>console.log( 'Group Indexes = " . $index . "' );</script>";
+//////                echo "<script>console.log( 'Grouped Looped = " . $formattedJob->find(2) . "' );</script>";//returns a collection instance with id = 2 if 2 passed as argument, nothing when 79 is although id = 79
+////////                echo "<script>console.log( 'Grouped Property = " . $formattedJob->all('locations') . "' );</script>";//returns a collection instance with id = 2 if 2 passed as argument, nothing when 79 is although id = 79
+//////
+//////            } else {
+//////                echo "<script>console.log( 'Grouped Property not found' );</script>";//returns a collection instance with id = 2 if 2 passed as argument, nothing when 79 is although id = 79
+//////
+//////
+//            }
+//            if($index == '05/28/2017'){
+////                echo "<script>console.log( 'Accessed Index = " . $index . "' );</script>";
+//                $count++;
+//
+////            $count = count($index == '05/03/2017');
+//                echo "<script>console.log( 'Count of indexes = " . $count . "' );</script>";
+//
+//
+
 
     public function compareValues($modifiedJobs, $value, $modifiedValue, $optionalValue1 = null, $optionalValue2 = null, $optionalValue3 = null, $optionalValue4 = null)
     {
