692be4f07f69f3149e5567205fbd46ca941bf587
Bernie Carroll <bernadettecar77@hotmail.com>
2017-07-22 19:52:55 +1000

    Styling Changes Login & ForgotPw

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,60 +4,66 @@
 
 @section('content')
 
-    <div>
-        <img src="{{ asset("/bower_components/AdminLTE/dist/img/ODIN-Logo.png") }}" alt="Odin Logo" height="60px" width="200px" style="position: absolute; left:30px; top:30px;"/>
-        <div class='col-lg-4 col-lg-offset-4' style="padding-top: 150px;">
-            @if (count( $errors ) > 0)
-                @foreach ($errors->all() as $error)
-                    <div class='bg-danger alert'>{{ $error }}</div>
-                @endforeach
-            @endif
+    <img src="{{ asset("/bower_components/AdminLTE/dist/img/ODIN-Logo.png") }}" alt="Odin Logo" height="60px" width="200px"
+         style="position: absolute; left:30px; top:30px;"/>
+    <div class="container" style="padding-top: 150px;">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2" >
+                @if (count( $errors ) > 0)
+                    @foreach ($errors->all() as $error)
+                        <div class='bg-danger alert'>{{ $error }}</div>
+                    @endforeach
+                @endif
+                <div class="panel panel-default" style="background-color: #eae6ee;">
+                    <div class="panel-heading"  style="color: white; background-color: #663974;">Login</div>
+                    <div class="panel-body">
+                        {{ Form::open(['role' => 'form','method' => 'POST']) }}
+                        <div style="background-color: #eae6ee;">
+                            <div class='form-group'>
+                                <label for="email" class="col-md-4 control-label">E-Mail</label>
+                                {{ Form::text('username', null, [
+                                'placeholder' => 'Email',
+                                 'class' => 'form-control',
+                                 'style' =>  'border: 1px solid #663974;
+                                    -webkit-text-fill-color: grey;
+                                    -webkit-box-shadow: 0 0 0px 1000px white inset;
+                                    transition: background-color 5000s ease-in-out 0s;'
+                                ]) }}
+                            </div>
 
-            {{ Form::open(['role' => 'form','method' => 'POST']) }}
-            <div style="height: 300px; background-color: #eae6ee; padding-top: 150px; padding: 100px 50px 70px 50px; border-radius: 15px;">
-                <div class='form-group'>
-                    {{--{{ Form::label('username', 'Email') }}--}}
-                    {{ Form::text('username', null, [
-                    'placeholder' => 'Email',
-                     'class' => 'form-control',
-                     'style' =>  'border: 1px solid #663974;
-                        -webkit-text-fill-color: grey;
-                        -webkit-box-shadow: 0 0 0px 1000px white inset;
-                        transition: background-color 5000s ease-in-out 0s;'
-                    ]) }}
-                </div>
+                            <div class='form-group'>
+                                <label for="email" class="col-md-4 control-label">Password</label>
+                                {{ Form::password('password', [
+                                'placeholder' => 'Password',
+                                 'class' => 'form-control login-input',
+                                'style' =>  'border: 1px solid #663974;
+                                    -webkit-text-fill-color: grey;
+                                    -webkit-box-shadow: 0 0 0px 1000px white inset;
+                                    transition: background-color 5000s ease-in-out 0s;'
+                                  ])
+                                }}
+                            </div>
 
-                <div class='form-group'>
-                    {{--{{ Form::label('password', 'Password') }}--}}
-                    {{ Form::password('password', [
-                    'placeholder' => 'Password',
-                     'class' => 'form-control login-input',
-                    'style' =>  'border: 1px solid #663974;
-                        -webkit-text-fill-color: grey;
-                        -webkit-box-shadow: 0 0 0px 1000px white inset;
-                        transition: background-color 5000s ease-in-out 0s;'
-                      ])
-                    }}
-                </div>
+                            <div class='form-group'>
+                                {{ Form::button('Create Account', ['class' => 'btn login-btns', 'style' => 'color: white; background-color: #663974;']) }}
+                                {{ Form::submit('Login', [
+                                    'class' => 'btn login-btns',
+                                     'style' => 'color: white;
+                                     background-color: #663974;
+                                     position: absolute;
+                                     right: 70px;'
+                                 ])
+                                 }}
+                            </div>
+                            <div style="text-align: center;">
+                                <a href='/reset/link' style="color: #663974; text-align: center;">Forgot Password?</a>
+                            </div>
+                        </div>
+                        {{ Form::close() }}
 
-                <div class='form-group'>
-                    {{ Form::button('Create Account', ['class' => 'btn login-btns', 'style' => 'color: white; background-color: #663974;']) }}
-                    {{ Form::submit('Login', [
-                        'class' => 'btn login-btns',
-                         'style' => 'color: white;
-                         background-color: #663974;
-                         position: absolute;
-                         right: 70px;'
-                     ])
-                     }}
-                </div>
-                <div style="text-align: center;">
-                    <a href='/reset/link' style="color: #663974; text-align: center;">Forgot Password?</a>
+                    </div>
                 </div>
             </div>
-            {{ Form::close() }}
-
         </div>
     </div>
-
 @stop
