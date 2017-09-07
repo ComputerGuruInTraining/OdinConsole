a931676bcc8071cc009ace118d764f6a2c046fba
Bernie Carroll <bernadettecar77@hotmail.com>
2017-09-07 13:17:58 +1000

    config

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://odinlitemgmt.azurewebsites.net/'),
 
     /*
     |--------------------------------------------------------------------------
@@ -103,7 +103,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => 'jieu573lcMTuiqpslkeox93ue75vo5ey',
 
     'cipher' => 'AES-256-CBC',
 
