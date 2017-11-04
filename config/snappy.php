15c5b0afc20e0159b0585dafa7fbe379fc6d432f
Bernie Carroll <bernadettecar77@hotmail.com>
2017-11-04 14:22:40 +1100

    Plugin required

--- config/snappy.php
+++ config/snappy.php
@@ -7,7 +7,7 @@ return array(
         'enabled' => true,
 //        'binary'  => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
 //       'binary' =>   '/usr/local/bin/wkhtmltopdf-amd64',
-        'binary'  => base_path('vendor/h4cc/wkhtmltopdf'),
+        'binary'  => base_path('app/Utilities/Plugins/wkhtmltopdf'),
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
