9867dc2ea8efc4b33fb0b3ec8fc733c1b54eb395
Bernie Carroll <bernadettecar77@hotmail.com>
2018-01-12 14:44:59 +1100

    footer sm

--- resources/views/footer_alt.blade.php
+++ resources/views/footer_alt.blade.php
@@ -1,18 +1,14 @@
 <footer class="footer-alt">
   <script src="//code.jquery.com/jquery-1.10.2.js"></script>
 
-    <!-- To the right -->
     <div style="background-color:#ffffff; font-size: small; margin-bottom: 15px;">
-        {{--<div class="login-pgs-footer">--}}
-            <strong>Copyright © {{currentYear()}}
+                <strong>Copyright © {{currentYear()}}
                 <a href="http://www.odincasemanagement.com/" style="color:#4d2970;" target="_blank">{{Config::get('constants.COMPANY_NAME')}}</a>
             </strong>  All rights reserved.
             <a href="/privacy" style="color:#4d2970;" target="_blank">  Privacy Policy</a>
-        {{--</div>--}}
     </div>
     <div>
         <p class="email-footer-alt">Contact: {{Config::get('constants.COMPANY_EMAIL')}}</p>
     </div>
-    <!-- Default to the left -->
 
 </footer>
